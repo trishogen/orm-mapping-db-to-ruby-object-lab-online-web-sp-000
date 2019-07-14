5303e8a6c0dba1b3b55dc09bfea289c262bc2186
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 02:56:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,6 +74,10 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
