dea555a463c9e232f386f66c9177b94eb4f8effe
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 02:58:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,9 +82,8 @@ class Student
     LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    row = DB[:conn].execute(sql)
+    self.new_from_db(row)
   end
 
   def save
