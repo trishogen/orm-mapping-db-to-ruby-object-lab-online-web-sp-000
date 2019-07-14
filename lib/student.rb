7fea2d61b646e40d222d4dfdc4448c684e890bd0
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 03:03:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,6 +86,18 @@ class Student
     self.new_from_db(row)
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
